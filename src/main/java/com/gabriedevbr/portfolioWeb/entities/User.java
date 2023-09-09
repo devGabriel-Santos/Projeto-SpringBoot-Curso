776c3635612ae6ad713ce91114efd0e38437bab9
Gabriel L <gabriellevindo15@gmail.com>
2023-09-09 12:25:23 -0300

    Project Created

---
+++ src/main/java/com/gabriedevbr/portfolioWeb/entities/User.java
@@ -0,0 +1,5 @@
+package com.gabriedevbr.portfolioWeb.entities;
+
+public class User {
+
+}
---
+++ src/test/java/com/gabriedevbr/portfolioWeb/PortfolioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriedevbr.portfolioWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
