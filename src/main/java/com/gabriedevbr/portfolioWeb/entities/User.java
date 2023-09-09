c87eda0fa3acbe411664b4aeebe861f2a7cf3633
Gabriel L <gabriellevindo15@gmail.com>
2023-09-09 14:07:30 -0300

    JpaRepository, dependency injection, database seeding

--- src/main/java/com/gabriedevbr/portfolioWeb/entities/User.java
+++ src/main/java/com/gabriedevbr/portfolioWeb/entities/User.java
@@ -1,5 +1,102 @@
 package com.gabriedevbr.portfolioWeb.entities;
 
-public class User {
+import java.io.Serializable;
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "tb_user")
+
+public class User implements Serializable {
+private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String email;
+	private Integer phone;
+	private Integer password;
+	
+	public User() {
+
+	}
+
+	public User(Integer id, String name, String email, Integer phone, Integer password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Integer phone) {
+		this.phone = phone;
+	}
+
+	public Integer getPassword() {
+		return password;
+	}
+
+	public void setPassword(Integer password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, name, password, phone);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Objects.equals(password, other.password) && Objects.equals(phone, other.phone);
+	}
+	
+	
+	
+	
 }
---
+++ src/main/java/com/gabriedevbr/portfolioWeb/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gabriedevbr.portfolioWeb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabriedevbr.portfolioWeb.entities.User;
+
+public interface UserRepository  extends JpaRepository<User, Integer>{
+	
+	
+
+}
