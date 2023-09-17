69729e6bf766968cab02d02d3bf6ffbd8007e0d4
Gabriel L <gabriellevindo15@gmail.com>
2023-09-17 20:54:28 -0300

    OrderStatus enum

--- src/main/java/com/gabriedevbr/portfolioWeb/config/TestConfig.java
+++ src/main/java/com/gabriedevbr/portfolioWeb/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.gabriedevbr.portfolioWeb.entities.Order;
 import com.gabriedevbr.portfolioWeb.entities.User;
+import com.gabriedevbr.portfolioWeb.entities.enums.OrderStatus;
 import com.gabriedevbr.portfolioWeb.repositories.OrderRepository;
 import com.gabriedevbr.portfolioWeb.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", 977777777, 123456);
 
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-20T19:53:07z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-08-20T19:53:07z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-20T19:53:07z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-08-20T19:53:07z"), OrderStatus.WAITING_PAYMENT, u1);
 		useRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
